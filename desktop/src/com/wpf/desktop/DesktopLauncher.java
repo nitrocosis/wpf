821cfac77496b1ec2f0093bb40b3340f32b0762d
Sarah Giovine <nitrocosis@gmail.comr>
2017-03-11 11:18:41 -0500

    Setup for desktop and Android

--- desktop/src/com/wpf/desktop/DesktopLauncher.java
+++ desktop/src/com/wpf/desktop/DesktopLauncher.java
@@ -5,8 +5,15 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.wpf.WPF;
 
 public class DesktopLauncher {
+
     public static void main(String[] arg) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "WPF";
+        config.useGL30 = false;
+        config.width = 640;
+        config.height = 360;
+        config.resizable = false;
+
         new LwjglApplication(new WPF(), config);
     }
 }
