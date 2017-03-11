df83e784a30f7f275c235b43d1d5756d199982ca
Sarah Giovine <nitrocosis@gmail.comr>
2017-03-11 11:08:39 -0500

    Initial LibGdx setup

---
+++ android/src/com/wpf/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.wpf;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.wpf.WPF;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new WPF(), config);
+	}
+}
---
+++ html/src/com/wpf/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package com.wpf.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import com.wpf.WPF;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new WPF();
+        }
+}
